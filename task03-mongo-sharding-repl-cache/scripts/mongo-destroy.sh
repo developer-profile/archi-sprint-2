9c55a51e67c0e8b10bdaa4c12696f6b8ae103274
Dimitrij Molokov <dimitrij@molokov.de>
2025-02-22 19:44:37 +0300

    Update scripts for task03

--- task03-mongo-sharding-repl-cache/scripts/mongo-destroy.sh
+++ task03-mongo-sharding-repl-cache/scripts/mongo-destroy.sh
@@ -10,8 +10,10 @@ NC=$(tput sgr0)
 ###
 echo -e "\n\n${RED}Destroying cluster with volumes${NC}\n\n"
 #
+cd ./task03-mongo-sharding-repl-cache/scripts/
 docker compose -f ../compose.yaml down
 docker compose -f ../compose.yaml down -v
 docker volume prune
 
 echo -e "\n\n${RED}Destroyed!${NC}\n\n"
+cd ../../
--- task03-mongo-sharding-repl-cache/scripts/mongo-init.sh
+++ task03-mongo-sharding-repl-cache/scripts/mongo-init.sh
@@ -10,6 +10,7 @@ NC=$(tput sgr0)
 ###
 echo -e "${GREEN}Starting cluster${NC}\n\n"
 #
+cd ./task03-mongo-sharding-repl-cache/scripts/
 docker compose -f ../compose.yaml up -d
 
 
@@ -125,3 +126,4 @@ echo -e "\n\n${GREEN}It's${NC} ${RED}finished!${NC}\n\n"
 #
 echo -e "\n\n${GREEN}To${NC} ${RED}destroy${NC} everything with volumes run ${RED}monga-destroy.sh${NC}\n\n"
 #
+cd ../../
